7c3a53e4f9a94662f6a37e59f1e09e821c60bb99
Ted Stein <karamarisan@gmail.com>
2011-05-13 13:43:58 -0500

    Redo MoreSettings as a PreferenceActivity. Changing output dir not yet working.

--- src/net/tedstein/AndroSS/MoreSettings.java
+++ src/net/tedstein/AndroSS/MoreSettings.java
@@ -1,26 +1,47 @@
 package net.tedstein.AndroSS;
 
-import android.app.Activity;
+import net.tedstein.AndroSS.util.RootUtils;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MoreSettings extends Activity {
+public class MoreSettings extends PreferenceActivity {
+    private static final String TAG = "AndroSS";
+    private PreferenceManager mPreferenceManager;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.more_settings);
+        addPreferencesFromResource(R.xml.more_settings);
+        mPreferenceManager = getPreferenceManager();
+        final Context context = this;
 
-        TextView goto_debug = (TextView)findViewById(R.id.tv_goto_debug);
-        final Context c = this;
-        goto_debug.setOnClickListener(new OnClickListener() {
+        Preference retry_root_check = mPreferenceManager.findPreference("retry_root_check");
+        retry_root_check.setOnPreferenceClickListener(new OnPreferenceClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(c, DebugInfo.class);
+            public boolean onPreferenceClick(Preference preference) {
+                RootUtils.showRootTestMessage(context);
+                return true;
+            }
+        });
+
+        Preference debug_info = mPreferenceManager.findPreference("debug_info");
+        debug_info.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent i = new Intent(context, DebugInfo.class);
                 startActivity(i);
+                return true;
             }
         });
     }
@@ -30,7 +51,28 @@ public class MoreSettings extends Activity {
     protected void onResume() {
         super.onResume();
 
-        TextView screenshot_dir_current = (TextView)findViewById(R.id.tv_screenshot_dir_current);
-        screenshot_dir_current.setText(AndroSSService.getOutputDir());
+        final Context context = this;
+        final EditTextPreference screenshot_dir =
+            (EditTextPreference)mPreferenceManager.findPreference("screenshot_dir");
+        final String old_output_dir = AndroSSService.getOutputDir();
+        screenshot_dir.setSummary(old_output_dir);
+        screenshot_dir.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String new_output_dir = (String)newValue;
+                if (AndroSSService.setOutputDir(new_output_dir)) {
+                    screenshot_dir.setSummary(new_output_dir);
+                } else {
+                    screenshot_dir.setSummary(old_output_dir);
+                    Toast.makeText(context,
+                            context.getString(R.string.change_output_error, new_output_dir),
+                            Toast.LENGTH_LONG);
+                }
+                return true;
+            }
+        });
+        EditText et = screenshot_dir.getEditText();
+        et.setSingleLine();
+        et.setText(old_output_dir);
     }
 }
