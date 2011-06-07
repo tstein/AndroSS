53b244d7a75a4fa58f198d1b4c5dc5edab210999
Ted Stein <karamarisan@gmail.com>
2011-06-07 13:44:51 -0500

    Compile native code -O3.

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS	:= -Wall -Wextra -Werror -Wno-unused-parameter -std=c99
+LOCAL_CFLAGS	:= -Wall -Wextra -Werror -Wno-unused-parameter -std=c99 -O3
 LOCAL_LDLIBS	:= -llog
 LOCAL_MODULE    := AndroSS_nbridge
 LOCAL_SRC_FILES := AndroSS_nbridge.c
@@ -12,7 +12,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
-LOCAL_CFLAGS	:= -Wall -Wextra -Werror -std=c99
+LOCAL_CFLAGS	:= -Wall -Wextra -Werror -std=c99 -O3
 LOCAL_LDLIBS	:= -llog
 LOCAL_MODULE	:= AndroSS
 LOCAL_SRC_FILES	:= AndroSS.c
